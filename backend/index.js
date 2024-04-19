43f0980d8ea7efb5f1369c5c65ab5d36894c7e77
sathwik-kadiyala <sathwikkadiyala@gmail.com>
2024-04-19 19:32:09 +0530

    final

--- backend/index.js
+++ backend/index.js
@@ -1,44 +1,50 @@
 const express = require('express');
-const app = express();
 const os = require('os');
-const port = 4000;
 const cors = require('cors');
 const nodemailer = require('nodemailer');
 const mongoose = require('mongoose');
+
 mongoose.connect('mongodb+srv://sathwikkadiyala:sathwik@cluster0.ekipjtb.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 
+const app = express();
 app.use(cors());
 app.use(express.static('public'));
+
+const port = 4000;
+
 const userSchema = new mongoose.Schema({
   email: String,
   username: String,
   dnaSequence: String
 });
 
-// Create a model based on the schema
+
 const UserModel = mongoose.model('User', userSchema);
+
+
 app.post('/send-email', express.json(), async (req, res) => {
   const { email, username,dnaSequence } = req.body;
 
   try {const user = new UserModel({ email, username, dnaSequence });
 
-  // Save the user document to MongoDB
+  
   await user.save();
 
     await sendEmail(email,username, dnaSequence);
     res.json({ message: 'Email sent successfully.' });
-  } catch (error) {
+
+  } 
+  catch (error) {
     console.error('Error sending email:', error);
     res.status(500).json({ message: 'Error sending email.' });
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server is listening at http://localhost:${port}`);
-});
+
 
 async function sendEmail(toEmail,username,  dnaSequence) {
+  
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
@@ -56,3 +62,7 @@ async function sendEmail(toEmail,username,  dnaSequence) {
 
   await transporter.sendMail(mailOptions);
 }
+
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
--- job/job.py
+++ job/job.py
@@ -7,6 +7,7 @@ received_dna_sequence = sys.argv[2]
 
 # Connect to MongoDB and retrieve stored DNA sequence
 client = MongoClient('mongodb+srv://sathwikkadiyala:sathwik@cluster0.ekipjtb.mongodb.net/?retryWrites=true&w=majority')
+
 db = client['test']
 collection = db['users']
 
@@ -21,6 +22,7 @@ if user_document and 'dnaSequence' in user_document:
     if received_dna_sequence == stored_dna_sequence:
         print('User authenticated. Running Spark job...')
         # Your Spark job logic here
+        
     else:
         print('Authentication failed. Exiting...')
         sys.exit(1)
